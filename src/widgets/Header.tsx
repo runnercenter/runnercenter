91f155bf5d38adc8316f7399b7d304b6ddabf744
A.Galimzhan <a.galimzhan@redmadrobot.kz>
2025-08-21 14:54:03 +0500

    Feat: Доработка header

--- src/widgets/Header.tsx
+++ src/widgets/Header.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import closeLogo from '../assets/icons/close.svg'
 import keyLogo from '../assets/icons/key.svg'
+import arrowDownLogo from '../assets/icons/arrow-down.svg'
 
 interface HeaderProps {
   className?: string;
@@ -8,6 +9,9 @@ interface HeaderProps {
 
 export function Header({ className = '' }: HeaderProps) {
   const [isAdOpen, setIsAdOpen] = useState(true);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const handleDropdownToggle = () => setIsDropdownOpen((prev) => !prev);
+  const handleDropdownClose = () => setIsDropdownOpen(false);
   return (
     <header className={`${className}`}>
       {isAdOpen && (
@@ -20,8 +24,16 @@ export function Header({ className = '' }: HeaderProps) {
       )}
       <div className='bg-[#061A84]'>
         <div className="w-[1240px] mx-auto text-[14px] flex py-3 items-center px-2 text-[#8D98D0]">
-          <div className='flex items-center gap-8'>
-            <div>О копмпании</div>
+          <div className='flex items-center gap-8 relative'>
+            <div className='cursor-pointer flex items-center gap-2 relative select-none' onClick={handleDropdownToggle} tabIndex={0} onBlur={handleDropdownClose}>
+              <div>О компании</div>
+              <img src={arrowDownLogo} alt="" className="cursor-pointer object-contain" />
+              {isDropdownOpen && (
+                <div className="absolute left-0 top-full mt-2 bg-[#061A84] shadow-lg rounded z-10 min-w-[160px] text-[#8D98D0]" onMouseLeave={handleDropdownClose}>
+                  <a href="#" className="block px-4 py-2 whitespace-nowrap">Отзывы</a>
+                </div>
+              )}
+            </div>
             <a href="#">Доставка и оплата</a>
             <a href="#">Возврат товара</a>
             <a href="#">Программа привилегий</a>
@@ -30,6 +42,14 @@ export function Header({ className = '' }: HeaderProps) {
           <div className='flex items-center ml-auto gap-3'>
             <img src={keyLogo} alt="" className="cursor-pointer object-contain" />
             <div>Личный кабинет</div>
+          </div>
+        </div>
+      </div>
+
+      <div className=''>
+        <div className="w-[1240px] mx-auto text-[14px] flex py-3 items-center px-2 text-[#8D98D0]">
+          <div>
+            <img src="/runnercenter/logo.png" />
           </div>
         </div>
       </div>
